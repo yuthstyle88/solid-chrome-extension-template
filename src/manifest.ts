cc03f9bf3de67a3f9a02d8e6e29cb231704a252e
yongyutjantaboot <yuth_style88@hotmail.com>
2025-06-16 18:48:39 +0700

    init for a new extension

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,21 +3,23 @@ import packageJson from "../package.json";
 
 // Convert from Semver (example: 0.1.0-beta6)
 const [major, minor, patch, label = "0"] = packageJson.version
-  // can only contain digits, dots, or dash
   .replace(/[^\d.-]+/g, "")
-  // split into version parts
   .split(/[.-]/);
 
-const manifest = defineManifest(async () => ({
+const version = `${major}.${minor}.${patch}.${label}`;
+
+const manifest = defineManifest(() => ({
   manifest_version: 3,
   name: packageJson.displayName ?? packageJson.name,
-  version: `${major}.${minor}.${patch}.${label}`,
+  version,
   description: packageJson.description,
   options_page: "src/pages/options/index.html",
   background: { service_worker: "src/pages/background/index.ts" },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icons/34x34.png",
+    default_icon: {
+      "34": "icons/34x34.png",
+    },
   },
   chrome_url_overrides: {
     newtab: "src/pages/newtab/index.html",
